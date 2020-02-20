b47c12d666496fac30e2b5a420b484f442607675
George Perdrizet <george@perdrizet.org>
2020-02-20 12:43:53 -0800

    Implemented recursive stratified sampling

--- python/fully_stratify_sample.py
+++ python/fully_stratify_sample.py
@@ -8,6 +8,9 @@ from mpi4py import MPI
 from scipy.stats import ks_2samp
 from sklearn.preprocessing import StandardScaler
 
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 def k_random_sample(data, k):
     # Takes a dataframe and an number of observations
     # returns new dataframe containing k from n pseudorandom
@@ -18,25 +21,27 @@ def k_random_sample(data, k):
     return data.iloc[indices]
 
 
-def ks_test(master_df, sample_ids, weather_variables):
+def ks_test(master_df, sample, weather_variables):
     
     ks_pvals = []
-    sample = master_df[master_df['ID'].isin(sample_ids)]
-    
+
     for variable in weather_variables:
         master_data = np.array(master_df[variable])
         sample_data = np.array(sample[variable])
 
-        ks_result = ks_2samp(master_data, sample_data)
-        ks_pvals.append(ks_result[1])
+        try:
+            ks_result = ks_2samp(master_data, sample_data)
+            
+        except ValueError:
+            ks_pvals.append(0.0)
+            
+        else:
+            ks_pvals.append(ks_result[1])
     
     return ks_pvals
 
 
-def replace_sample_subset(master_df, sample_ids, fraction):
-    # rebuild sample
-    sample = master_df[master_df.ID.isin(sample_ids)]
-    
+def replace_sample_subset(master_df, sample, fraction):  
     # split sample into positive and negative
     sample_positive = sample[sample['ignition'] == 1]
     sample_negative = sample[sample['ignition'] == 0]
@@ -94,9 +99,6 @@ def make_starting_sample(master_df, sample_size):
     # sample data
     no_ignitions_sample = k_random_sample(no_ignitions, no_ignition_sample_size)
     ignitions_sample = k_random_sample(ignitions, ignition_sample_size)
-    
-    print("Starting sample ignitions: {}".format(len(ignitions_sample)))
-    print("Starting sample, no ignition: {}".format(len(no_ignitions_sample)))
 
     # combine
     sample = no_ignitions_sample.append(ignitions_sample)
@@ -122,6 +124,26 @@ status = MPI.Status()   # get MPI status object
 
 data_file = '../data/training_data/1992-2015_training_data_raw.csv'
 
+dtypes={
+    'air.sfc': float,
+    'air.2m': float,
+    'apcp': float,
+    'crain': float,
+    'rhum.2m': float,
+    'dpt.2m': float,
+    'pres.sfc': float,
+    'uwnd.10m': float,
+    'vwnd.10m': float,
+    'veg': float,
+    'prate': float,
+    'vis': float,
+    'lat': float,
+    'lon': float,
+    'weather_bin_month': int,
+    'weather_bin_year': int,
+    'ignition': float
+}
+
 weather_variables = [
     'air.sfc',
     'air.2m', 
@@ -135,14 +157,22 @@ weather_variables = [
     'veg',
     'prate',
     'vis',
+    'lat',
+    'lon',
+    'weather_bin_month',
+    'weather_bin_year'
 ]
 
-sample_size = 50000
-fraction = 0.1
+file_read_chunksize = 1000
+sample_size = 10000
+fraction = 0.01
 sample_round = 0
-target_pval = 0.9
+target_pval = 0.7
+target_pvals = [target_pval] * len(weather_variables)
+old_total_distance = len(weather_variables)
 
 excluded_observations = []
+sample_ids = []
 
 output_file_base_name = "../data/stratified_training_data/1992-2015_training_data_raw_n" \
     +str(sample_size) \
@@ -150,48 +180,36 @@ output_file_base_name = "../data/stratified_training_data/1992-2015_training_dat
     +str(target_pval) \
     +"."
 
+for worker_num in range(0,size):
+    if rank == worker_num:
+        name = MPI.Get_processor_name()
+        print("{}-{} loading dataframe".format(name, rank))
+        
+        master_df = pd.DataFrame()
+        for chunk in pd.read_csv(data_file, dtype=dtypes, chunksize=file_read_chunksize):
+            master_df = pd.concat([master_df, chunk], ignore_index=True)        
+            
+        master_df.insert(0, 'ID', range(0, len(master_df)))
+        print("{}-{} done loading".format(name, rank))
+
 start_time = time.time()
 
 if rank == 0:
-    print("Number of processes: {}".format(size))
-    # Load & prep data
-    master_df = pd.read_csv(data_file, low_memory=False)
-    master_df.insert(0, 'ID', range(0, len(master_df)))
-
-    # Shuffel row order
-    master_df = master_df.sample(frac=1).reset_index(drop=True)
-    
-    # Scale variables of intrest
-    scaler = StandardScaler()
-    scaled_data = pd.DataFrame(scaler.fit_transform(master_df[weather_variables]), columns=weather_variables)
-    
-    # Add back variables we did not scale
-    scaled_data['lat'] = master_df['lat']
-    scaled_data['lon'] = master_df['lon']
-    scaled_data['weather_bin_month'] = master_df['weather_bin_month']
-    scaled_data['weather_bin_year'] = master_df['weather_bin_year']
-    scaled_data['ignition'] = master_df['ignition']
-    
-    # Add unscaled variable we want to stratify back to list
-    weather_variables.append = [
-        'weather_bin_month',
-        'weather_bin_year',
-        'lat',
-        'lon'
-    ]
-
-    # take starting sample and calculate ks
+    # Grab and score starting sample
     sample = make_starting_sample(master_df, sample_size)
     sample_ids = sample['ID']
-    ks_pvals = ks_test(master_df, sample_ids, weather_variables)
-    target_pvals = [target_pval] * len(weather_variables)
+    ks_pvals = ks_test(master_df, sample, weather_variables)
     distances = np.subtract(target_pvals, ks_pvals)
-    old_total_distance = sum(np.square(distances)) ** 0.5
+    total_distance = sum(np.maximum(distances, 0))
+    old_total_distance = total_distance
     
+    print()
+    print("Number of processes: {}".format(size))
     print("Done loading and prepping data")
     print("Master df size: {}".format(len(master_df)))
     print("Sample size: {}".format(len(sample)))
-    print("Starting total distance: {}".format(old_total_distance))
+    print("Starting total distance: {}".format(np.round(old_total_distance,2)))
+    print()
 
     while True:
         # Master process executes code below
@@ -202,13 +220,10 @@ if rank == 0:
         if tag == tags.READY:
             # assemble workunit
             workunit = [
-                weather_variables,
                 old_total_distance,
-                fraction,
                 sample_round,
                 sample_ids,
                 excluded_observations,
-                target_pval
             ]
 
             # send workunit to nodes
@@ -221,16 +236,22 @@ if rank == 0:
             worker_total_distance = result[1]
             worker_sample_round = result[2]
             worker_sample_ids = result[3]
+            worker_name = result[4]
+            
             dT = np.round(((time.time() - start_time) / 3600),2)
 
             # if distance from worker wins, update
             if worker_total_distance < old_total_distance and worker_sample_round == sample_round:
-                print("Winning distance from worker {} round {}: {}, dT: {}".format(
-                    source, 
+                print("Winning distance from {}-{}, round {}: {}, dT: {}".format(
+                    worker_name,
+                    source,
                     worker_sample_round, 
-                    np.round(worker_total_distance, 4),
+                    np.round(worker_total_distance, 2),
                     dT)
                 )
+                
+                print()
+                
                 sample_ids = worker_sample_ids
                 old_total_distance = worker_total_distance
 
@@ -239,67 +260,72 @@ if rank == 0:
                 if all(p_vals >= 0.9 for p_vals in worker_ks_pvals) and worker_sample_round == sample_round:
                     # write winner to disk
                     print("Winner winner chicken dinner, round: {}".format(sample_round))
+                    print()
                     output_file = str(output_file_base_name)+str(sample_round)+'.csv'
                     sample = master_df[master_df.ID.isin(sample_ids)]
                     sample.to_csv(output_file, index=False)
-                    sample_round += 1
 
-                    # reset for next workunit
+                    # Exclude members of the winning sample from future samples
                     excluded_observations = sample['ID']
                     master_df = master_df[~master_df.ID.isin(excluded_observations)]
+                    
+                    # Reset and on to the next round
                     sample = make_starting_sample(master_df, sample_size)
-                    sample_ids = sample['ID']
-                    ks_pvals = ks_test(master_df, sample_ids, weather_variables)
+                    ks_pvals = ks_test(master_df, sample, weather_variables)
                     distances = np.subtract(target_pvals, ks_pvals)
-                    old_total_distance = sum(np.square(distances)) ** 0.5
-                    print("New starting distance: {}".format(old_total_distance))
+                    total_distance = sum(np.maximum(distances, 0))
 
 else:
     # Worker processes execute code below
-    master_df = pd.read_csv(data_file, low_memory=False)
-    master_df.insert(0, 'ID', range(0, len(master_df)))
-    
     name = MPI.Get_processor_name()
+    
+    # Desyncronize node starts by 1-60 seconds
+    time.sleep((random.randint(10, 600) / 10))
     
     while True:
         comm.send(None, dest=0, tag=tags.READY)
         workunit = comm.recv(source=0, tag=MPI.ANY_TAG, status=status)
         tag = status.Get_tag()
-
-        # unpack workunit
-        weather_variables = workunit[0]
-        old_total_distance = workunit[1]
-        fraction = workunit[2]
-        sample_round = workunit[3]
-        sample_ids = workunit[4]
-        excluded_observations = workunit[5]
-        target_pval = workunit[6]
         
         if tag == tags.START:
+            # Unpack workunit
+            old_total_distance = workunit[0]
+            sample_round = workunit[1]
+            sample_ids = workunit[2]
+            excluded_observations = workunit[3]
+            
+#             print("{} starting with   {}".format(
+#                 rank, 
+#                 np.round(old_total_distance,2)
+#             ))
+            
+#             print()
+        
             master_df = master_df[~master_df.ID.isin(excluded_observations)]
-            sample = replace_sample_subset(master_df, sample_ids, fraction)
-            target_pvals = [target_pval] * len(weather_variables)
-
-            # Do the work here
-            for i in range(randint(10, 20)):
-                sample_ids = sample['ID']
-                ks_pvals = ks_test(master_df, sample_ids, weather_variables)
+            sample = master_df[master_df.ID.isin(sample_ids)]
+            
+            for i in range(1):
+                sample = replace_sample_subset(master_df, sample, fraction)
+                ks_pvals = ks_test(master_df, sample, weather_variables)
                 distances = np.subtract(target_pvals, ks_pvals)
-                total_distance = sum(np.square(distances)) ** 0.5
+                total_distance = sum(np.maximum(distances, 0))
+                
+#                 print("{} returning dist. {}".format(
+#                     rank, 
+#                     np.round(total_distance,2)
+#                 ))
                 
                 if total_distance < old_total_distance:
-                    # if we win assemble result
-                    result = []
-                    result.append(ks_pvals)
-                    result.append(total_distance)
-                    result.append(sample_round)
-                    result.append(sample_ids)
-
-                    # send result to headnode   
-                    comm.send(result, dest=0, tag=tags.DONE)
-                    
+                    # if we win, break the for loop to call home imediatly                    
                     break
+                    
+            # after n trials assemble result
+            result = []
+            result.append(ks_pvals)
+            result.append(total_distance)
+            result.append(sample_round)
+            result.append(sample_ids)
+            result.append(name)
 
-                else:
-                    # If we did not win, go back to our last winner, resample and move on
-                    sample = replace_sample_subset(master_df, sample_ids, fraction)
+            # send result to headnode
+            comm.send(result, dest=0, tag=tags.DONE)
